6e563694adaf38896269a1341bad1c384aefbb89
Carlton Mwalumba <carltonmwalumba@gmail.com>
2025-06-10 07:26:07 +0000

    change 3

--- models/example/stage_orders.sql
+++ models/example/stage_orders.sql
@@ -9,8 +9,8 @@ SELECT
     o.Quantity,
     o.Order_Date
 FROM
-    raw.orders o
+    raw.Orders o
 JOIN
-    {{ ref("stg_customers") }} c ON o.Customer_ID = c.Customer_ID
+    {{ ref("stg_cust") }} c ON o.Customer_ID = c.Customer_ID
 JOIN
-    raw.products p ON o.Product_ID = p.Product_ID
+    raw.Products p ON o.Product_ID = p.Product_ID
